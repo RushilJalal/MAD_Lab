916707197aafe49dcaa2880257c0fe1bfe533bb5
rushil <rushiljalal@gmail.com>
2025-01-17 12:30:24 +0530

    Lifecycle methods app

--- Lab2/Lifecycle_app/app/src/main/java/com/example/lifecycle_app/MainActivity.java
+++ Lab2/Lifecycle_app/app/src/main/java/com/example/lifecycle_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.lifecycle_app;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,77 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Lifecycle_app";
+    private TextView lifecycleTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        lifecycleTextView = findViewById(R.id.lifecycleTextView);
+
+        Log.d(TAG, "onCreate() called");
+        updateTextView("onCreate() called");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+        updateTextView("onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+        updateTextView("onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+        updateTextView("onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+        updateTextView("onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+        updateTextView("onDestroy() called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart() called");
+        updateTextView("onRestart() called");
+    }
+
+    private void updateTextView(String message){
+        String currentText = lifecycleTextView.getText().toString();
+        String updatedText = currentText + "\n" + message;
+        lifecycleTextView.setText(updatedText);
     }
 }
